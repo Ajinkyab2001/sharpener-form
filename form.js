19ecb0f89026eb223443b9d8ac2cd3235d39abbf
Ajinkyab2001 <ajinkyab2001@gmail.com>
2023-03-24 12:37:56 +0530

    edit button

--- form.js
+++ form.js
@@ -7,7 +7,6 @@ function myfunc(event){
         dates : document.getElementById("date").value,
         times : document.getElementById("time").value,
      }
-
    
      let user_records = [];
    
@@ -15,27 +14,35 @@ function myfunc(event){
         "user_details": myobj,
      
       })
-
       showUserOnScreen(myobj)
      localStorage.setItem(myobj.email_id ,JSON.stringify(user_records));
-
 }
-
-
 function showUserOnScreen(myobj){
-    const parentItem = document.getElementById('ddd');
-    const childItem = document.createElement('div');
-    childItem.textContent = myobj.full_name +' '+myobj.email_id+' ';
-    
+    const parentElem = document.getElementById('ddd');
+    const childElem = document.createElement('div');
+    childElem.textContent = myobj.full_name +' '+myobj.email_id+' ';
+
     const deleteButton = document.createElement('input')
     deleteButton.type = "button"
     deleteButton.value = 'delete'
     deleteButton.onclick = () => {
         localStorage.removeItem(myobj.email_id)
-        parentItem.removeItem(childItem)
+        parentElem.removeChild(childElem)
     }
-    childItem.appendChild(deleteButton)
 
-    parentItem.appendChild(childItem);
+    const edit = document.createElement('input')
+    edit.type = "button"
+    edit.value = 'edit'
+    edit.onclick = () => {
+        localStorage.removeItem(myobj.email_id)
+        parentElem.removeChild(childElem)
+        document.getElementById("Name").value = myobj.full_name
+        document.getElementById("Email").value = myobj.email_id
+    }
+
+     
+    childElem.appendChild(deleteButton)
+    childElem.appendChild(edit)
+
+    parentElem.appendChild(childElem);
 }
-
